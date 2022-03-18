eefeb4be75d80b4431f4cd8eb76cc86abcf22438
nanashi019 <101402057+nanashi019@users.noreply.github.com>
2022-03-19 08:19:46 +0900

    Add files via upload

--- intermediate/__proto__.js
+++ intermediate/__proto__.js
@@ -6,6 +6,21 @@ subObj.__proto__ = superObj; /// 상속지정
 console.log('subObj.subVal ==>', subObj.subVal);
 console.log('subObj.superVal ==>', subObj.superVal);//부모꺼
 
-var sub2Obj = Object.create(superObj);
+var sub2Obj = Object.create(superObj);//부모
 sub2Obj.sub2Val = 'sub2';
 console.log('sub2Obj.superVal ==>', sub2Obj.superVal);//부모꺼
+
+
+var kim = {
+  name: 'kim',
+  first: 10, second: 20,
+  sum: function(){return this.first+this.second}
+}
+console.log('kim,sum() : ', kim.sum());
+
+var lee = {
+  name: 'lee',
+  first: 10, second: 20
+}
+lee.__proto__ = kim;
+console.log('lee,sum()', lee.sum());
