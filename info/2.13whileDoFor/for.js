e8fc4194b63eba7786c408dab71a001511245061
nanashi019 <101402057+nanashi019@users.noreply.github.com>
2022-03-22 17:23:21 +0900

    Add files via upload

---
+++ info/2.13whileDoFor/for.js
@@ -0,0 +1,7 @@
+
+
+for (let j = 0; j <= 10; j++){
+  if(j%2 == 0){
+  console.log(j);
+  }
+}
---
+++ info/2.13whileDoFor/while.js
@@ -0,0 +1,17 @@
+let i = 0;
+while (i < 3){
+  console.log(i);
+  i = i + 0.5;
+}
+
+let j = 3
+while (j){
+  console.log(j--);
+}
+
+
+let k = 0
+while (k < 3) {
+console.log(`number ${k}!`);
+k++;
+}
